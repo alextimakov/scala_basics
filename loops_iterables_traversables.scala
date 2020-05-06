bd4409dec321d51e2b59db917b4996b97a24270c
alextimakov <alextimakoff94@gmail.com>
2020-05-06 20:00:15 +0300

    added examples of iterables and operations with them

--- loops_iterables_traversables.scala
+++ loops_iterables_traversables.scala
@@ -372,3 +372,88 @@ val list = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
 stringBuilder.append("I want all numbers 6-12: ")
 list.filter(it => it > 5 && it < 13).addString(stringBuilder, ",")
 stringBuilder.mkString should be("I want all numbers 6-12: 6,7,8,9,10,11,12")
+
+// The next trait from the top in the collections hierarchy is `Iterable` 
+// All methods in this trait are defined in terms of an abstract method `iterator` which yields the collection's elements one by one 
+// The `foreach` method from trait `Traversable` is implemented in `Iterable` in terms of `iterator`
+def foreach[U](f: Elem => U): Unit = {
+  val it = iterator
+  while (it.hasNext) f(it.next())
+}
+
+// A few subclasses of `Iterable` override this standard implementation of `foreach` in `Iterable` to  provide a more efficient implementation
+// `foreach` is the basis of the implementation of all operations in `Traversable`, so its performance matters
+// Some common iterables are `Set`, `List`, `Vector`, `Stacks` and `Stream` 
+// Iterator has two important methods: `hasNext`, which answers whether the iterator has another element available, 
+// and `next` which returns the next element in the iterator
+val list = List(3, 5, 9, 11, 15, 19, 21)
+val it = list.iterator
+if (it.hasNext) {
+  it.next should be(3)
+}
+
+// `grouped` will return fixed-size `Iterable` chunks of an `Iterable`
+val list = List(3, 5, 9, 11, 15, 19, 21, 24, 32)
+val it = list grouped 3
+it.next() should be(List(3, 5, 9))
+it.next() should be(List(11, 15, 19))
+it.next() should be(List(21, 24, 32))
+
+// `sliding` will return an `Iterable` that shows a sliding window of an `Iterable`
+val it = list sliding 3
+it.next() should be(List(3, 5, 9))
+it.next() should be(List(5, 9, 11))
+it.next() should be(List(9, 11, 15))
+
+// `sliding` can take the size of the window as well the size of the step during each iteration
+val it = list sliding (3, 3)
+it.next() should be(List(3, 5, 9))
+it.next() should be(List(11, 15, 19))
+it.next() should be(List(21, 24, 32))
+
+// `takeRight` is the opposite of 'take' in `Traversable`. It retrieves the last elements of an `Iterable`
+(list takeRight 3) should be(List(21, 24, 32))
+
+// `dropRight` will drop a specified number of elements from the right
+val list = List(3, 5, 9, 11, 15, 19, 21, 24, 32)
+(list dropRight 3) should be(List(3, 5, 9, 11, 15, 19))
+
+// `zip` will stitch two iterables into an iterable of pairs of corresponding elements from both iterables
+val xs = List(3, 5, 9)
+val ys = List("Bob", "Ann", "Stella")
+(xs zip ys) should be(List((3, "Bob"), (5, "Ann"), (9, "Stella")))
+
+// If two Iterables aren't the same size, then `zip` will only zip what can be paired
+val xs = List(3, 5, 9)
+val ys = List("Bob", "Ann")
+(xs zip ys) should be(List((3, "Bob"), (5, "Ann")))
+
+// If two Iterables aren't the same size, then `zipAll` can provide fillers for what it couldn't find a complement for
+val xs = List(3, 5, 9)
+val ys = List("Bob", "Ann")
+(xs zipAll (ys, -1, "?")) should be(List((3, "Bob"), (5, "Ann"), (9, "?")))
+
+val xt = List(3, 5)
+val yt = List("Bob", "Ann", "Stella")
+(xt zipAll (yt, -1, "?")) should be(List((3, "Bob"), (5, "Ann"), (-1, "Stella")))
+
+// `zipWithIndex` will zip an `Iterable` with its integer index
+val xs = List("Manny", "Moe", "Jack")
+xs.zipWithIndex should be(List(("Manny", 0), ("Moe", 1), ("Jack", 2)))
+
+// `sameElements` will return true if the two `Iterables` produce the same elements in the same order
+val xs = List("Manny", "Moe", "Jack")
+val ys = List("Manny", "Moe", "Jack")
+xs.iterator.sameElements(ys) should be(true)
+
+val xt = List("Manny", "Moe", "Jack")
+val yt = List("Manny", "Jack", "Moe")
+xt.iterator.sameElements(yt) should be(false)
+
+val xs1 = Set(3, 2, 1, 4, 5, 6, 7)
+val ys1 = Set(7, 2, 1, 4, 5, 6, 3)
+xs1.iterator.sameElements(ys1) should be(true)  // `set` specific behavior 
+
+val xt1 = Set(1, 2, 3)
+val yt1 = Set(3, 2, 1)
+xt1.iterator.sameElements(yt1) should be(false)
