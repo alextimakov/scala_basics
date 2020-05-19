7736b10a29f16fc40bc534dae3e62d11ce96b398
alextimakov <alextimakoff94@gmail.com>
2020-05-19 21:04:32 +0300

    added examples of null elements

--- literals.scala
+++ literals.scala
@@ -88,3 +88,51 @@ f.toString should be("\\")
 // one-line String literals are surrounded by quotation marks
 val a = "To be or not to be"
 a should be("To be or not to be")
+
+// empty values types
+// Scala's `null` is the same as in Java - any reference type can be `null`, like Strings, Objects, or your own classes
+// just like Java, value types like Ints can't be `null`
+// `Null` is a trait whose only instance is `null`, It is a subtype of all reference types, but not of value types 
+// its purpose in existing is to make it so reference types can be assigned null and value types can't
+// `Nothing` is a trait that is guaranteed to have zero instances. It is a subtype of all other types. It has two main reasons for existing: 
+// to provide a return type for methods that never return normally (i.e. a method that always throws an exception). 
+// to provide a type for `Nil`
+// `Unit` in Scala is the equivalent of `void` in Java
+// It's used in a function's signature when that function doesn't return a value
+// `Nil` is just an empty list, exactly like the result of `List()`. It is of type `List[Nothing]`. 
+// And since we know there are no instances of `Nothing`, we now have a list that is statically verifiable as empty
+
+// An empty list can be represented by another nothing value: `Nil`
+List() === Nil shouldBe true
+
+// `None` is the counterpart to `Some`, used when you're using Scala's `Option` class to help avoid `null` references
+None === None shouldBe true
+None eq None shouldBe true
+
+// `None` can be converted to string or an empty list
+assert(None.toString === "None")
+None.toList === Nil shouldBe true
+
+// `None` is considered empty
+assert(None.isEmpty === true)
+
+// `None` can be cast to `Any`, `AnyRef` or `AnyVal`
+None.asInstanceOf[Any] === None shouldBe true
+None.asInstanceOf[AnyRef] === None shouldBe true
+None.asInstanceOf[AnyVal] === None shouldBe true
+
+// `None` can be used with `Option` instead of null references
+val optional: Option[String] = None
+assert(optional.isEmpty === true)
+assert(optional === None)
+
+// `Some` is the opposite of `None` for `Option` types
+val optional: Option[String] = Some("Some Value")
+assert((optional == None) === false, "Some(value) should not equal None")
+assert(optional.isEmpty === false, "Some(value) should not be empty")
+
+// `Option.getOrElse` can be used to provide a default in the case of `None`
+val optional: Option[String] = Some("Some Value")
+val optional2: Option[String] = None
+assert(optional.getOrElse("No Value") === "Some Value", "Should return the value in the option")
+assert(optional2.getOrElse("No Value") === "No Value", "Should return the specified default value")
